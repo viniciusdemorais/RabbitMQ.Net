9f94c2a19987c61e1a52c148ffd9f357167582fe
Vinicius de Morais <vinicius.morais@opah.com.br>
2020-03-10 17:41:28 -0300

    First commit

---
+++ NewTask/NewTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Collections.Generic;
+
+class NewTask
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            var fakeArray = new List<string>() { "first . ", "second .. ", "third ... ", "fourth ... ", "fifth ..... " };
+            foreach (var item in fakeArray)
+            {
+
+                channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(item + message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "", routingKey: "task_queue", basicProperties: properties, body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    private static string GetMessage(string[] args)
+    {
+        return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+    }
+}
